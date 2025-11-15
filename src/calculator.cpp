cb4c58dd967daa3f3d784b66a1b18fbca5033bb8
root <root@DESKTOP-HUMBB82.localdomain>
2025-11-15 21:34:30 +0100

    Add comments to calculator

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -1,10 +1,12 @@
 #include "../include/calculator.h"
 #include <stdexcept>
 
+// Addition
 int Calculator::add(int a, int b) {
     return a + b;
 }
 
+// Division
 double Calculator::divide(double a, double b) {
     if (b == 0) {
         throw std::runtime_error("Division by zero!");
